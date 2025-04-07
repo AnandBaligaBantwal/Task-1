160c387e4c3dd1e95f536400dd86a175d1f67755
Anand Baliga Bantwal <abaliga190@gmail.com>
2025-04-07 13:05:02 +0200

    created updated df replacing misisng with median values

--- Python Code.py
+++ Python Code.py
@@ -2,4 +2,17 @@ import pandas as pd
 import numpy as np
 
 df = pd.read_csv("C:\\Users\\abali\\Downloads\\marketing_campaign.csv", sep="\t")
+
+df = pd.DataFrame(df)
+
 df.head()
+
+df.drop_duplicates()
+
+df.isnull().sum()
+
+income_median = df["Income"].median()
+
+updated_df = df["Income"].fillna(income_median)
+
+updated_df.isnull().sum()
